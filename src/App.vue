da6acf52c024ef50047be4399fd4add1182a2ef0
Marcin Eljasiewicz <m.eljasiewicz@gmail.com>
2022-12-04 20:30:57 +0100

    added new sections in overview

--- src/App.vue
+++ src/App.vue
@@ -2,38 +2,47 @@
   <div class="container">
     <nav class="navigation">
       <div class="logo">
-        <img src="./images/logo.png" alt="logo">
+        <img src="./images/logo.png" alt="logo" />
         <h3>Payte</h3>
       </div>
 
       <router-link class="link" to="/overview">
-        <img src="./images/overview.png" alt="icon">
+        <img src="./images/Icon.svg" alt="icon" />
         Overview
       </router-link>
 
-      <router-link class="link"  to="/buysell">
-        <img src="./images/buy.png" alt="icon">
+      <router-link class="link" to="/buysell">
+        <img src="./images/buy.png" alt="icon" />
         Buy / Sell
       </router-link>
       <router-link class="link" to="/wallets">
-        <img src="./images/wallet.png" alt="icon">
+        <img src="./images/wallet.png" alt="icon" />
         Wallets
       </router-link>
       <router-link class="link" to="/bundles">
-        <img src="./images/bundles.png" alt="icon">
+        <img src="./images/bundles.png" alt="icon" />
         Bundles
       </router-link>
       <router-link class="link" to="/reporting">
-        <img src="./images/reporting.png" alt="icon">
+        <img src="./images/reporting.png" alt="icon" />
         Reporting
       </router-link>
       <router-link class="link" to="/community">
-        <img src="./images/community.png" alt="icon">
+        <img src="./images/community.png" alt="icon" />
         Community
       </router-link>
+      <div class="favorites">
+        <h3 class="fav">
+          Favorites
+          <img src="./images/dots.png" alt="icon" />
+        </h3>
+        <div></div>
+      </div>
     </nav>
-  <router-view />
-</div>
+    <section class="content">
+      <router-view />
+    </section>
+  </div>
 </template>
 
 <script>
@@ -42,46 +51,85 @@ export default {
 };
 </script>
 
-
-
 <style scoped>
 .container {
-  font-family: 'Source Sans Pro', sans-serif;
+  width: 1440px;
+  font-family: "Source Sans Pro", sans-serif;
   display: flex;
   flex-direction: row;
 }
-.link{
+.content{
+  width: 1176px;
+  display: flex;
+  flex-direction: column;
+}
+.link {
+  display: flex;
+  align-items: center;
   text-decoration: none;
   box-sizing: border-box;
   height: 44px;
   width: 244px;
-  background: #F7F7F9;
+  background: #f7f7f9;
   margin: 3px;
   padding: 2px;
+  font-size: 16px;
+  font-weight: 600;
+  color: hsla(255, 75%, 6%, 0.4);
+}
 
+.link img {
+  width: 21px;
+  height: 21px;
+  margin: 15px;
 }
 
 .link:hover,
 .link.router-link-active,
 .link.router-link-exact-active {
   border: 1px solid rgb(227, 225, 225);
-   background-color: white;
-   cursor: pointer;
- }
+  background-color: white;
+  cursor: pointer;
+  font-weight: bold;
+  color: black;
+  border-radius: 4px;
+}
 
-.logo{
+.logo {
   display: flex;
+  align-items: center;
+  margin-bottom: 20px;
 }
 .logo img {
-  background: none;
+  margin: 10px;
+  max-height: 24px;
+  width: auto;
+}
+.logo h3 {
+  font-size: 24px;
 }
 .navigation {
   width: 264px;
-  background: #F7F7F9;
+  height: 900px;
+  background: #f7f7f9;
   display: flex;
   flex-direction: column;
 }
 
-
-
+.navigation .fav {
+  margin: 20px;
+  padding: 0 10px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  font-size: 14px;
+  font-weight: 700;
+  
+}
+.favorites {
+  font-size: 14px;
+  margin-top: 10px;
+  border-top: 1px solid rgb(227, 225, 225);
+  color: hsla(255, 75%, 6%, 0.4);
+}
 </style>
--- src/components/CoinsListPage.vue
+++ src/components/CoinsListPage.vue
@@ -1,8 +1,42 @@
 <template>
-  <div v-for="coin in coins" :key="coin.id">
-    <p>{{ coin.name }}</p>
-    <p>{{ coin.symbol }}</p>
-  </div>
+  <main class="main">
+    <header class="header">
+      <input class="search" type="search" />
+      <div class="buttons">
+        <button class="btn">
+          <img src="../images/bell.png" alt="icon bell" />
+        </button>
+        <button class="btn">
+          <img class="btn" src="../images/menu.png" alt="icon menu" />
+        </button>
+      </div>
+    </header>
+    <section class="first_section">
+      <div class="balance">
+        <div class="title">
+          <h3>Current Balance</h3>
+          <button>
+            <img src="../images/dots.png" alt="dots icon" />
+          </button>
+        </div>
+        <h4>$21 432.23</h4>
+        <div class="change">
+          <img src="../images/arrow.png" alt="arrow" />
+          <p>12% vs last month</p>
+        </div>
+        <div class="btn_section">
+          <button class="btn_invest">Quick Invest</button>
+          <button class="btn_report">Show Report</button>
+        </div>
+      </div>
+      <div class="summary">
+        <div class="title">
+          <h3>Summary</h3>
+          <img src="../images/dots.png" alt="dots icon" />
+        </div>
+      </div>
+    </section>
+  </main>
 </template>
 
 <script>
@@ -17,7 +51,7 @@ export default {
 
     const res = await fetch(url + qString);
     const parsedRes = await res.json();
-    console.log(parsedRes);
+
     coins.value = parsedRes.data;
     console.log(coins.value);
 
@@ -26,4 +60,130 @@ export default {
 };
 </script>
 
-<style></style>
+<style scoped>
+.main {
+  padding: 20px 50px;
+  display: flex;
+  flex-direction: column;
+}
+.header {
+  height: 48px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+.buttons {
+  width: 130px;
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.search {
+  width: 232px;
+}
+
+.search,
+.btn {
+  background: #f7f7f9;
+  height: 48px;
+  border: 1px solid #f7f7f9;
+  border-radius: 8px;
+}
+
+.first_section {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+  margin-top: 35px;
+}
+
+.balance {
+  box-sizing: border-box;
+  display: flex;
+  flex-direction: column;
+  background: #f7f7f9;
+  border: 1px solid #f7f7f9;
+  border-radius: 8px;
+  width: 509px;
+  height: 336px;
+  padding: 15px 20px;
+}
+
+.title {
+  display: flex;
+  align-items: center;
+  justify-content: space-between;
+}
+
+.title button {
+  border: none;
+  background: #f7f7f9;
+}
+
+.title h3 {
+  font-size: 20px;
+}
+
+.balance h4 {
+  margin: 5px 0;
+  color: black;
+  font-size: 56px;
+}
+
+.change {
+  width: 160px;
+  height: 24px;
+  display: flex;
+  justify-content: space-between;
+  align-items: center;
+  font-size: 18px;
+  color: #2dc78f;
+}
+
+.change img {
+  height: 18px;
+  width: 18px;
+}
+
+.btn_section {
+  margin-top: 60px;
+  display: flex;
+  align-items: center;
+}
+
+.btn_section button {
+    border: none;
+}
+
+.btn_invest {
+  background: #7445fb;
+  color: white;
+  width: 109px;
+  height: 40px;
+  margin-right: 20px;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: 600;
+}
+
+.btn_report {
+    background: white;
+  color: #7445fb;
+  width: 112px;
+  height: 40px;
+  border-radius: 4px;
+  font-size: 14px;
+  font-weight: 600;
+}
+
+.summary {
+  box-sizing: border-box;
+  background: #f7f7f9;
+  border: 1px solid #f7f7f9;
+  border-radius: 8px;
+  width: 499px;
+  height: 336px;
+  padding: 15px 20px;
+}
+</style>
